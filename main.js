abff28d84b5c993c042c85fedba8c76778ff9b30
Sparticle999 <alexscruton@gmail.com>
2016-03-23 18:25:08 +0000

    Update main.js

--- main.js
+++ main.js
@@ -26,63 +26,38 @@ function refreshStats(){
 
 function activeMain() {
     "use strict";
-
     document.getElementById("mainTab").style.color = "#dddddd";
     document.getElementById("statsTab").style.color = "#222222";
     document.getElementById("settingsTab").style.color = "#222222";
     document.getElementById("shopTab").style.color = "#222222";
-    document.getElementById("main").style.display = "block";
-    document.getElementById("stats").style.display = "none";
-    document.getElementById("settings").style.display = "none";
-    document.getElementById("shop").style.display = "none";
+
 }
 
 function activeStats() {
     "use strict";
-    document.getElementById("mainTab").style.backgroundColor = "transparent";
-    document.getElementById("statsTab").style.backgroundColor = "#222222";
-    document.getElementById("settingsTab").style.backgroundColor = "transparent";
-    document.getElementById("shopTab").style.backgroundColor = "transparent";
     document.getElementById("mainTab").style.color = "#222222";
     document.getElementById("statsTab").style.color = "#dddddd";
     document.getElementById("settingsTab").style.color = "#222222";
     document.getElementById("shopTab").style.color = "#222222";
-    document.getElementById("main").style.display = "none";
-    document.getElementById("stats").style.display = "block";
-    document.getElementById("settings").style.display = "none";
-    document.getElementById("shop").style.display = "none";
+
 }
 
 function activeSettings() {
     "use strict";
-    document.getElementById("mainTab").style.backgroundColor = "transparent";
-    document.getElementById("statsTab").style.backgroundColor = "transparent";
-    document.getElementById("settingsTab").style.backgroundColor = "#222222";
-    document.getElementById("shopTab").style.backgroundColor = "transparent";
     document.getElementById("mainTab").style.color = "#222222";
     document.getElementById("statsTab").style.color = "#222222";
     document.getElementById("settingsTab").style.color = "#dddddd";
     document.getElementById("shopTab").style.color = "#222222";
-    document.getElementById("main").style.display = "none";
-    document.getElementById("stats").style.display = "none";
-    document.getElementById("settings").style.display = "block";
-    document.getElementById("shop").style.display = "none";
+
 }
 
 function activeShop() {
     "use strict";
-    document.getElementById("mainTab").style.backgroundColor = "transparent";
-    document.getElementById("statsTab").style.backgroundColor = "transparent";
-    document.getElementById("settingsTab").style.backgroundColor = "transparent";
-    document.getElementById("shopTab").style.backgroundColor = "#222222";
     document.getElementById("mainTab").style.color = "#222222";
     document.getElementById("statsTab").style.color = "#222222";
     document.getElementById("settingsTab").style.color = "#222222";
-    document.getElementById("shopTab").style.color = "#dddddd";
-    document.getElementById("main").style.display = "none";
-    document.getElementById("stats").style.display = "none";
-    document.getElementById("settings").style.display = "none";
-    document.getElementById("shop").style.display = "block";
+    document.getElementById("shopTab").style.color = "#ffffff";
+
 }
 
 function gainOre(){
