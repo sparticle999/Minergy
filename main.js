b8eef5366a44af46e48e742c14c9f0fc73aefd99
Sparticle999 <alexscruton@gmail.com>
2016-03-24 21:53:39 +0000

    Update main.js

--- main.js
+++ main.js
@@ -472,7 +472,7 @@ function upgradeAutoSell(){
 	if(money >= 1000000){
 		money -= 1000000;
 		autosell = 1;
-		document.getElementById("upgradeAutoSell").className = "btn-info disabled"
+		document.getElementById("upgradeAutoSell").className = "btn-info hidden"
 	}
 }
 
