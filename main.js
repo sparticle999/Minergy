dd40553d5619a0aaa6a8ba06c5d8f453272479a6
Sparticle999 <alexscruton@gmail.com>
2016-03-24 22:23:45 +0000

    Update main.js

--- main.js
+++ main.js
@@ -500,7 +500,7 @@ function pulseMine4(){
 function upgradeAutoSell(){
 	if(money >= 1000000){
 		money -= 1000000;
-		autosell = 1;
+		autosell === 1;
 		document.getElementById("upgradeAutoSell").className = "btn-info hidden"
 	}
 }
