a084e2679d839308753eb19f7d2c17df8f5633d4
Sparticle999 <alexscruton@gmail.com>
2016-03-28 14:09:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,13 +25,12 @@ function refreshStats(){
 	document.getElementById("lithium").innerHTML = lithium;
 	document.getElementById("diamond").innerHTML = diamond;
 	document.getElementById("energy").innerHTML = energy;
-	document.getElementById("money").innerHTML = money;
 	numWithCommas(money);
+	document.getElementById("money").innerHTML = money;
 }
 
 function numWithCommas(x) {
-    var moneyComma =  x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    document.getElementById("money").innerHTML = moneyComma;
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function checkEnergyPositive(){
